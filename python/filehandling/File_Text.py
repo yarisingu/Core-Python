b5164bed515ec1ebd4c900adc852cc9327d0137f
yarisingu <yarisingutarun@gmail.com>
2023-05-11 23:07:34 +0530

    file handling

---
+++ python/filehandling/File_Text.py
@@ -0,0 +1,8 @@
+# Open a file in read mode
+file = open("myfile.txt", "r")
+
+# Open a file in write mode
+file = open("myfile.txt", "w")
+
+# Open a file in append mode
+file = open("myfile.txt", "a")
