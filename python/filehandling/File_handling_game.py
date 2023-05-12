3c34a0be94ef65a312d234162694f93ec355d400
yarisingu <yarisingutarun@gmail.com>
2023-05-12 16:38:35 +0530

    file handling game

---
+++ python/filehandling/File_handling_game.py
@@ -0,0 +1,48 @@
+import random
+
+def play_game():
+    # Generate a random number between 1 and 100
+    number = random.randint(1, 100)
+    
+    # Initialize variables
+    score = 0
+    high_score = read_high_score()
+    
+    print("Welcome to the High Score Game!")
+    print("The current high score is:", high_score)
+    print("Guess a number between 1 and 100.")
+    
+    while True:
+        guess = int(input("Enter your guess: "))
+        score += 1
+        
+        if guess == number:
+            print("Congratulations! You guessed the number in", score, "attempts.")
+            
+            if score < high_score:
+                print("You've achieved a new high score!")
+                write_high_score(score)
+            else:
+                print("Try again to beat the high score.")
+            
+            break
+        elif guess < number:
+            print("Too low. Try again.")
+        else:
+            print("Too high. Try again.")
+
+def read_high_score():
+    try:
+        with open("high_score.txt", "r") as file:
+            high_score = int(file.read())
+    except FileNotFoundError:
+        high_score = float('inf')
+    
+    return high_score
+
+def write_high_score(score):
+    with open("high_score.txt", "w") as file:
+        file.write(str(score))
+
+# Start the game
+play_game()
